correcting choice of attack sign, folliation done

adversarial_attack.py
@@ -20,6 +20,30 @@ class AdversarialAttack(GeometricModel):
         """
         raise NotImplementedError()
 
+    
+    def attack_sign(
+        self,
+        init_point: torch.Tensor,
+        perturbation: torch.Tensor,
+        zero_value: float=1.,
+    ) -> torch.Tensor:
+        """Compute the sign of the attack to decrease the likelihood of the target.
+
+        Args:
+            init_point (torch.Tensor): point on where the attack take place with shape (bs, d)
+
+        Returns:
+            torch.Tensor: Batch tensor of the sign
+        """
+
+        max_likelihood_attacked, max_indices = torch.max(self.proba(init_point + perturbation), dim=1)
+        max_likelihood_origin = self.proba(init_point).gather(1, max_indices.unsqueeze(1)).squeeze(1)
+
+        perturbation_sign = torch.sign(max_likelihood_origin - max_likelihood_attacked)  # TODO: sum, or else ?
+        perturbation_sign[perturbation_sign==0] = zero_value
+        
+        return perturbation_sign
+
         
     def test_attack(self, budget, test_points):
         """Computes multiple attacks to check the fooling
@@ -118,7 +142,7 @@ class StandardTwoStepSpectralAttack(AdversarialAttack):
         first_step = first_step.reshape(input_sample.shape)
 
         """Computing first step's sign."""
-        first_step_sign = torch.sign(-self.proba(input_sample + first_step).log().sum(1) + self.proba(input_sample).log().sum(1))  # TODO: sum, or else ?
+        first_step_sign = self.attack_sign(input_sample, first_step)
         first_step = torch.einsum('z, z... -> z...', first_step_sign, first_step)
         # TODO: since less budget, we might go in the wrong direction ( close to the frontiers )
 
@@ -136,6 +160,7 @@ class StandardTwoStepSpectralAttack(AdversarialAttack):
         # print(first_step.T @ second_step)
         """Computing second step's sign."""
         second_step_sign = torch.einsum('z..., z... -> z', first_step, second_step).sign()
+        second_step_sign[second_step_sign==0] = 1
         second_step = torch.einsum('z, z... -> z...', second_step_sign, second_step)
 
         # optimal_ratio = ((second_step - first_step).T @ G @ second_step) / ((second_step + first_step).T @ G @ (second_step + first_step))
@@ -163,11 +188,10 @@ class OneStepSpectralAttack(AdversarialAttack):
 
         G = self.local_data_matrix(input_sample)
         e, v = torch.linalg.eigh(G)  # value, vector
-        perturbation = v[:, :, -1]  # be careful, it isn't intuitive -> RTD
+        perturbation = v[..., -1]  # be careful, it isn't intuitive -> RTD
         norm = torch.linalg.vector_norm(perturbation, ord=2, dim=-1, keepdim=True)
         perturbation = budget * perturbation / norm
         perturbation = perturbation.reshape(input_sample.shape)
-        """Computing first step's sign."""
-        perturbation_sign = torch.sign(-self.proba(input_sample + perturbation).log().sum(1) + self.proba(input_sample).log().sum(1))  # TODO: sum, or else ?
+        perturbation_sign = self.attack_sign(input_sample, perturbation)
         perturbation = torch.einsum('z, z... -> z...', perturbation_sign, perturbation)
         return input_sample + perturbation

foliation.py
@@ -26,17 +26,24 @@ class Foliation(GeometricModel):
         """
         def f(t, y):
             J = self.jac_proba(torch.tensor(y).float().unsqueeze(0)).squeeze(0).detach()
-            e = null_space(J) 
-            # print(f'e.shape = {e}\ny.shape = {y}')
-            # if len(e[0])==0: print(J)
-            if len(e[0]) == 0:
-                e = torch.tensor([[0.,0.]]).numpy()
-            return e[:,0]
+            a, b = J[0]
+            e = torch.tensor([b, -a])
+            # e = J[0]
+            e = e / e.norm(2)
+            # e = null_space(J) 
+            # if len(e[0]) == 0:
+            #     print(e, J)
+            #     e = torch.tensor([0.,0.]).numpy()
+            # else:
+            #     e = e[:,0]
+            return e.numpy()
 
-        leaf = solve_ivp(f, t_span=(0, 1), y0=init_point).y
-        # print(f'leaf shape = {leaf.shape}')
-
-        return leaf
+        leaf = solve_ivp(f, t_span=(0, 0.5), y0=init_point, method='RK23').y
+        leaf_back = solve_ivp(f, t_span=(0, -0.5), y0=init_point, method='RK23').y
+        # print(f'leaf shape = {leaf.shape}') 
+        
+        return torch.cat((torch.tensor(leaf_back).flip(1)[:,:-1], torch.tensor(leaf)), dim=1)
+        # return leaf
 
     def plot(self, leaves=True, eigenvectors=True, transverse=False):
         """Plot the leaves on the input space.
@@ -74,5 +81,5 @@ class Foliation(GeometricModel):
             savepath = savepath + '_eigen'
         if transverse:
             savepath = savepath + '_trans'
-        plt.savefig(savepath + '.pdf', format='pdf')
-        plt.show()
+        # plt.savefig(savepath + '.pdf', format='pdf')
+        # plt.show()

main.py
@@ -1,4 +1,3 @@
-from operator import xor
 from os import makedirs, path
 import torch
 import torchvision.datasets as datasets
@@ -11,15 +10,16 @@ from mnist_networks import medium_cnn
 from xor_networks import xor_net
 from xor_datasets import XorDataset
 from foliation import Foliation
+from torch import nn
 
 if __name__ == "__main__":
-    dataset_name = ["MNIST", "XOR"][1]
-    num_samples = int(1e2)
-    task = ["", "plot-attack", "plot-attacks-2D", "fooling-rates", "plot-leaves"][-1]
+    dataset_name = ["MNIST", "XOR"][0]
+    num_samples = int(200)
+    task = ["", "plot-attack", "plot-attacks-2D", "fooling-rates", "plot-leaves"][3]
 
     if dataset_name == "MNIST":
         checkpoint_path = './checkpoint/medium_cnn_10.pt'
-        network = medium_cnn(checkpoint_path)
+        network = medium_cnn(checkpoint_path, non_linearity=nn.Sigmoid())
         network_score = medium_cnn(checkpoint_path, score=True)
 
         normalize = transforms.Normalize((0.1307,), (0.3081,))
@@ -32,6 +32,7 @@ if __name__ == "__main__":
         )
     elif dataset_name == "XOR":
         checkpoint_path = './checkpoint/xor_net_10.pt'
+        # checkpoint_path = './checkpoint/xor_net_sigmoid_22.pt'
         network = xor_net(checkpoint_path)
         network_score = xor_net(checkpoint_path, score=True)
 
@@ -66,23 +67,24 @@ if __name__ == "__main__":
         input_points = torch.stack([input_space[idx][0] for idx in random_indices])
 
     if task == "plot-attack":
-        two_step_attack = STSSA.compute_attack(input_points, budget=28)
+        two_step_attack = STSSA.compute_attack(input_points, budget=1)
         plt.matshow(input_points[0][0] - two_step_attack.detach().numpy()[0][0])
         plt.show()
         one_step_attack = OSSA.compute_attack(
                                 input_points,
-                                budget=1e-3
+                                budget=1
                             )
 
-        plt.matshow(input_points[0][0] - two_step_attack.detach().numpy()[0][0])
+        plt.matshow(input_points[0][0] - one_step_attack.detach().numpy()[0][0])
         plt.show()
 
         plt.matshow(two_step_attack.detach().numpy()[0][0] - one_step_attack.detach().numpy()[0][0])
         plt.show()
     
     if task == "fooling-rates":
-        STSSA.save_fooling_rates(input_points, step=0.01, end=10)
-        OSSA.save_fooling_rates(input_points, step=0.01, end=10)
+        MAX_BUDGET = 10
+        STSSA.save_fooling_rates(input_points, step=0.1, end=MAX_BUDGET)
+        OSSA.save_fooling_rates(input_points, step=0.1, end=MAX_BUDGET)
         plt.legend()
         savedirectory = f"./output/{dataset_name}/"
         if not path.isdir(savedirectory):
@@ -93,7 +95,9 @@ if __name__ == "__main__":
         plt.clf()
 
     if task == "plot-attacks-2D":
+        foliation.plot(eigenvectors=False)
         STSSA.plot_attacks_2D(test_points=input_points,budget=0.1)
+        foliation.plot(eigenvectors=False)
         OSSA.plot_attacks_2D(test_points=input_points, budget=0.1)
     
     if task == "plot-leaves":

mnist_networks.py
@@ -2,17 +2,17 @@ import torch
 import torch.nn as nn
 
 
-def medium_cnn(checkpoint_path: str = "", num_classes: int=10, score: bool=False) -> nn.Module:
+def medium_cnn(checkpoint_path: str = "", num_classes: int=10, score: bool=False, non_linearity=nn.ReLU()) -> nn.Module:
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     net = nn.Sequential(
         nn.Conv2d(1, 32, 3, 1),
-        nn.ReLU(inplace=True),
+        non_linearity,
         nn.Conv2d(32, 64, 3, 1),
-        nn.ReLU(inplace=True),
+        non_linearity,
         nn.MaxPool2d(2),
         nn.Flatten(),
         nn.Linear(9216, 128),
-        nn.ReLU(inplace=True),
+        non_linearity,
         nn.Linear(128, num_classes),
         nn.LogSoftmax(dim=1) if not score else nn.Sequential(),
     )

mnist_training.py
@@ -14,8 +14,8 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 from mnist_networks import medium_cnn
-from model_manifold.data_matrix import batch_data_matrix_trace_rank
-from model_manifold.plot import save_ranks, save_mean_trace, save_images
+# from model_manifold.data_matrix import batch_data_matrix_trace_rank
+# from model_manifold.plot import save_ranks, save_mean_trace, save_images
 
 
 def train_epoch(
@@ -45,18 +45,18 @@ def train_epoch(
                 )
             )
             steps.append(batch_idx)
-            batch_traces, batch_ranks = batch_data_matrix_trace_rank(
-                model, reference_batch
-            )
+            # batch_traces, batch_ranks = batch_data_matrix_trace_rank(
+            #     model, reference_batch
+            # )
             # batch_FIM_traces, batch_FIM_ranks = batch_fisher_matrix_trace_rank(
             #     model, reference_batch
             # )
-            traces.append(batch_traces)
-            ranks.append(batch_ranks)
+            # traces.append(batch_traces)
+            # ranks.append(batch_ranks)
         optimizer.step()
     steps = torch.tensor(steps)
-    ranks = torch.stack(ranks, dim=1)
-    traces = torch.stack(traces, dim=1)
+    # ranks = torch.stack(ranks, dim=1)
+    # traces = torch.stack(traces, dim=1)
     return steps, ranks, traces
 
 
@@ -89,7 +89,7 @@ def test(model: nn.Module, loader: DataLoader) -> float:
 def mnist_loader(batch_size: int, train: bool) -> DataLoader:
     loader = torch.utils.data.DataLoader(
         datasets.MNIST(
-            "data_augmented",
+            "data",
             train=train,
             download=False,
             transform=transforms.Compose(
@@ -122,17 +122,16 @@ def emnist_loader(batch_size: int, train: bool) -> DataLoader:
     )
     idx = torch.randint(dataset.data.shape[0]-1, (16,))
     idx = torch.arange(0, 31 ,3) + 31*10
-    save_images(dataset.data[idx,...], f'./data_augmented/visualisation_{"train" if train else "test"}', predictions=[dataset.classes[i] for i in dataset.targets[idx,...].int()])
+    # save_images(dataset.data[idx,...], f'./data_augmented/visualisation_{"train" if train else "test"}', predictions=[dataset.classes[i] for i in dataset.targets[idx,...].int()])
 
     return loader
 
 
 def exemplar_batch(batch_size: int, train: bool) -> torch.Tensor:
-    dataset = datasets.EMNIST(
-        "data_augmented",
+    dataset = datasets.MNIST(
+        "data",
         train=train,
         download=False,
-        split='letters',
         transform=transforms.Compose(
             [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
         ),
@@ -170,11 +169,12 @@ if __name__ == "__main__":
     output_dir = Path(args.output_dir).expanduser()
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    model = medium_cnn(num_classes=27)  # 26 letters and 1 N/A
+    # model = medium_cnn(num_classes=27)  # 26 letters and 1 N/A
+    model = medium_cnn(num_classes=10, non_linearity=nn.Sigmoid())
     optimizer = optim.SGD(model.parameters(), lr=args.lr)
 
-    train_loader = emnist_loader(args.batch_size, train=True)
-    test_loader = emnist_loader(args.batch_size, train=False)
+    train_loader = mnist_loader(args.batch_size, train=True)
+    test_loader = mnist_loader(args.batch_size, train=False)
 
     global_steps = []
     global_ranks = []
@@ -192,13 +192,13 @@ if __name__ == "__main__":
     global_steps = torch.cat(global_steps, dim=0)
     global_ranks = torch.cat(global_ranks, dim=1)
     global_traces = torch.cat(global_traces, dim=1)
-    save_mean_trace(
-        global_steps,
-        global_traces,
-        output_dir / "traces_medium_cnn.pdf",
-    )
-    save_ranks(
-        global_steps,
-        global_ranks,
-        output_dir / "ranks_medium_cnn.pdf",
-    )
+    # save_mean_trace(
+    #     global_steps,
+    #     global_traces,
+    #     output_dir / "traces_medium_cnn.pdf",
+    # )
+    # save_ranks(
+    #     global_steps,
+    #     global_ranks,
+    #     output_dir / "ranks_medium_cnn.pdf",
+    # )