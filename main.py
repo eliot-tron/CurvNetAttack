df2c29c2ad6c9352928a4c4cca27913c54de6127
eliot-tron <eliottron@gmail.com>
2023-04-17 17:57:52 +0200

    range, savedirectory, TSSA and other minor stuff

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
 from adversarial_attack import (OneStepSpectralAttack,
-                                StandardTwoStepSpectralAttack)
+                                TwoStepSpectralAttack)
 from adversarial_attack_plots import compare_fooling_rates, compare_inf_norm, plot_attacks_2D, plot_curvature_2D
 from mnist_networks import medium_cnn
-from xor_networks import xor_net
+from xor_networks import xor_net, xor_net_old
 from xor_datasets import XorDataset
 from foliation import Foliation
 from torch import nn
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         "--dataset",
         type=str,
         default="MNIST",
-        choices=['MNIST', 'XOR'],
+        choices=['MNIST', 'XOR', 'XOR-old'],
         help="Dataset name to be used.",
     )
     parser.add_argument(
@@ -68,6 +68,21 @@ if __name__ == "__main__":
         default=None,
         help="Path to (budget, test_point, attack_vectors) if you had them precomputed."
     )
+    parser.add_argument(
+        "--range",
+        type=float,
+        metavar=("a","b"),
+        nargs=2,
+        default=[0.,1.],
+        help="Range for the generated datapoints for the Xor task. Datapoints will lie in [a,b)²."
+    )
+    parser.add_argument(
+        "--savedirectory",
+        type=str,
+        metavar='path',
+        default='./output/',
+        help="Path to the directory to save the outputs in."
+    )
     
     args = parser.parse_args()
     
@@ -81,7 +96,11 @@ if __name__ == "__main__":
     non_linearity = args.nl
     start_index = args.startidx
     attack_paths = args.attacks
+    xor_data_range = args.range
     batch_size = 125
+    savedirectory = args.savedirectory + ("" if args.savedirectory[-1] == '/' else '/') + f"{dataset_name}/{task}/"
+    if not path.isdir(savedirectory):
+        makedirs(savedirectory)
 
     if not args.random:
         seed = 42
@@ -97,12 +116,12 @@ if __name__ == "__main__":
     if dataset_name == "MNIST":
         MAX_BUDGET = 10
         STEP_BUDGET = 0.1
-    elif dataset_name == "XOR":
-        MAX_BUDGET = 1
-        STEP_BUDGET = 0.01
+    elif dataset_name[:3] == "XOR":
+        MAX_BUDGET = 0.5
+        STEP_BUDGET = 0.005
     
-    if task == "plot-curvature":
-        dataset_name = "XOR"
+    if task == "plot-curvature" and dataset_name[:3] != 'XOR':
+        raise ValueError(f'Plot curvature requires a 2D dataset and not {dataset_name}.')
 
     if dataset_name == "MNIST":
         if non_linearity == 'Sigmoid':
@@ -138,6 +157,26 @@ if __name__ == "__main__":
         input_space = XorDataset(
             nsample=10000,
             discrete=False,
+            range=xor_data_range,
+        )
+        print("dataset loaded")
+    elif dataset_name == "XOR-old":
+        if non_linearity == 'Sigmoid':
+            checkpoint_path = './checkpoint/old/xor_net_sigmoid_20.pt'
+            # print("/!\\ "*5 + "\nSigmoid for Xor not working.")
+            non_linearity = nn.Sigmoid()
+        elif non_linearity == 'ReLU':
+            checkpoint_path = './checkpoint/old/xor_net_relu_10.pt'
+            non_linearity = nn.ReLU()
+        print("Loading net")
+        network = xor_net_old(checkpoint_path, non_linearity=non_linearity)
+        network_score = xor_net_old(checkpoint_path, score=True, non_linearity=non_linearity)
+        print("Network loaded")
+
+        input_space = XorDataset(
+            nsample=10000,
+            discrete=False,
+            range=xor_data_range,
         )
         print("dataset loaded")
 
@@ -151,7 +190,7 @@ if __name__ == "__main__":
 
     print(f"network to {device} done")
 
-    STSSA = StandardTwoStepSpectralAttack(
+    TSSA = TwoStepSpectralAttack(
                 network=network,
                 network_score=network_score,
             )
@@ -215,7 +254,7 @@ if __name__ == "__main__":
     if task == "plot-attack":
         plt.matshow(input_points[0][0])
         plt.show()
-        two_step_attack = STSSA.compute_attack(input_points, budget=1)
+        two_step_attack = TSSA.compute_attack(input_points, budget=1)
         plt.matshow(input_points[0][0] - two_step_attack.detach().numpy()[0][0])
         plt.show()
         one_step_attack = OSSA.compute_attack(
@@ -231,9 +270,6 @@ if __name__ == "__main__":
     
     
     if task == "save-attacks":
-        savedirectory = f"./output/{dataset_name}/attacked_points/"
-        if not path.isdir(savedirectory):
-            makedirs(savedirectory)
         savename = f"attacks_nsample={num_samples}_start={start_index}"
         savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
 
@@ -244,7 +280,7 @@ if __name__ == "__main__":
 
         for batch_index, batch in enumerate(batched_input_points):
             print(f"Batch number {batch_index} starting...")
-            STSSA.save_attack(
+            TSSA.save_attack(
                 test_points=batch,
                 budget_step=STEP_BUDGET,
                 budget_max=MAX_BUDGET,
@@ -259,10 +295,7 @@ if __name__ == "__main__":
             torch.cuda.empty_cache()
     
     if task == "fooling-rates":
-        savedirectory = f"./output/{dataset_name}/"
-        if not path.isdir(savedirectory):
-            makedirs(savedirectory)
-        savename = f"fooling_rates_compared_nsample={num_samples}_start={start_index}"
+        savename = f"fooling_rates_compared_nsample={num_samples}_start={start_index}_nl={non_linearity}"
         savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
 
         if device.type == 'cuda' and attack_paths is None:
@@ -273,17 +306,14 @@ if __name__ == "__main__":
         for batch_index, batch in enumerate(batched_input_points):
             print(f"Batch number {batch_index} starting...")
             compare_fooling_rates(
-                [STSSA, OSSA],
+                [TSSA, OSSA],
                 batch,
                 budget_range=budget_range,
-                savepath=savepath + f"_batch={batch_index}",
+                savepath=savepath + f"_batch={batch_index}" + (f"I={xor_data_range}" if dataset_name == 'XOR' else ""),
                 attack_vectors=attack_vectors
             )
 
     if task == "inf-norm":
-        savedirectory = f"./output/{dataset_name}/"
-        if not path.isdir(savedirectory):
-            makedirs(savedirectory)
         savename = f"inf_norm_compared_nsample={num_samples}"
         savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
 
@@ -295,7 +325,7 @@ if __name__ == "__main__":
         for batch_index, batch in enumerate(batched_input_points):
             print(f"Batch number {batch_index} starting...")
             compare_inf_norm(
-                [STSSA, OSSA],
+                [TSSA, OSSA],
                 batch,
                 budget_range=budget_range,
                 savepath=savepath,
@@ -304,17 +334,26 @@ if __name__ == "__main__":
 
     if task == "plot-attacks-2D":
         foliation.plot(eigenvectors=False)
-        plot_attacks_2D(STSSA, test_points=input_points,budget=0.1)
-        foliation.plot(eigenvectors=False)
-        plot_attacks_2D(OSSA, test_points=input_points, budget=0.1)
+        plot_attacks_2D(TSSA, test_points=input_points,budget=0.1, color='blue')
+        # foliation.plot(eigenvectors=False)
+        plot_attacks_2D(OSSA, test_points=input_points, budget=0.1, color='orange')
+        # plt.legend()
+        savename = f"plot_attacks_2D_budget=1e-1_nsample={num_samples}_nl={non_linearity}"
+        savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
+        plt.savefig(savepath + '.pdf', format='pdf')
     
     if task == "plot-leaves":
         foliation.plot(eigenvectors=False)
     
     if task == "plot-curvature":
-        plot_curvature_2D(STSSA)
+        plot_curvature_2D(TSSA)
         foliation.plot(eigenvectors=False, transverse=True)
+        plt.xlim(0., 1.)
+        plt.ylim(0., 1.)
+        savename = f"plot_curvature_nl={non_linearity}"
+        savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
+        plt.savefig(savepath + f'.pdf', dpi=None, transparent=True)
         plt.show()
     
     # num_samples = 1
-    # STSSA.test_jac_proba(input_points)
+    # TSSA.test_jac_proba(input_points)
