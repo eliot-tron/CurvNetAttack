fixing dtype and debuging geodesic computation

adversarial_attack.py
@@ -238,10 +238,11 @@ class AdversarialAutoAttack(AdversarialAttack):
 
         """
         # adversary = AutoAttack(self.network_score, norm='L2', eps=budget, version='custom', attacks_to_run=['apgd-ce'], device=self.device, verbose=False)
-        adversary = AutoAttack(self.network_score, norm='L2', eps=budget, version='standard', device=self.device, verbose=False)
+        adversary = AutoAttack(self.network_score.float(), norm='L2', eps=budget, version='standard', device=self.device, verbose=False)
         labels = torch.argmax(self.proba(input_sample), dim=-1)
-        x_adv = adversary.run_standard_evaluation(input_sample, labels, bs=250) 
-        return x_adv
+        x_adv = adversary.run_standard_evaluation(input_sample.clone().float(), labels, bs=250) 
+        self.network_score.to(self.dtype) # to avoid messing with other attacks
+        return x_adv.to(self.dtype)
 
 
 class APGDAttack(AdversarialAttack):
@@ -258,10 +259,11 @@ class APGDAttack(AdversarialAttack):
         :returns: attacked point as a torch tensor (bs, d)
 
         """
-        adversary = AutoAttack(self.network_score, norm='L2', eps=budget, version='custom', attacks_to_run=['apgd-ce'], device=self.device, verbose=False)
+        adversary = AutoAttack(self.network_score.float(), norm='L2', eps=budget, version='custom', attacks_to_run=['apgd-ce'], device=self.device, verbose=False)
         labels = torch.argmax(self.proba(input_sample), dim=-1)
-        x_adv = adversary.run_standard_evaluation(input_sample, labels, bs=250) 
-        return x_adv
+        x_adv = adversary.run_standard_evaluation(input_sample.clone().float(), labels, bs=250)
+        self.network_score.to(self.dtype) # to avoid messing with other attacks
+        return x_adv.to(self.dtype)
 
 
 class GeodesicSpectralAttack(AdversarialAttack):
@@ -286,6 +288,11 @@ class GeodesicSpectralAttack(AdversarialAttack):
         else:
             _, v = torch.linalg.eigh(G)  # value, vector, in ascending order
             init_speed = v[..., -1]  # be careful, it isn't intuitive -> RTD
+        norm = torch.linalg.vector_norm(init_speed, ord=2, dim=-1, keepdim=True)
+        init_speed = budget * init_speed / norm
+        init_speed = init_speed.reshape(input_sample.shape)
+        init_speed_sign = self.attack_sign(input_sample, init_speed)
+        init_speed = torch.einsum('z, z... -> z...', init_speed_sign, init_speed)
 
         x_adv = self.geodesic(input_sample, init_speed, budget)
 

adversarial_attack_plots.py
@@ -194,8 +194,8 @@ def plot_contour_2D(adversarial_attack):
 def plot_curvature_2D(adversarial_attack):
     """Plot the extrinsic curvature for a 2D input manifold and 1D leaves.
     """
-    adversarial_attack.network = adversarial_attack.network.to(torch.double)
-    xs = torch.linspace(0, 1, steps=100).double()
+    adversarial_attack.network = adversarial_attack.network.to(adversarial_attack.device)
+    xs = torch.linspace(0, 1, steps=100).to(adversarial_attack.dtype)
     grid = torch.cartesian_prod(xs, xs)
 
     G_1 = adversarial_attack.local_data_matrix(grid)

foliation.py
@@ -24,8 +24,9 @@ class Foliation(GeometricModel):
         :returns: the curve gamma(t) with shape (bs, n, d)
 
         """
+        
         def f(t, y):
-            J = self.jac_proba(torch.tensor(y).float().unsqueeze(0)).squeeze(0).detach()
+            J = self.jac_proba(torch.tensor(y).to(self.device).to(self.dtype).unsqueeze(0)).squeeze(0).detach()
             a, b = J[0]
             if transverse:
                 e = torch.tensor([a, b])
@@ -65,7 +66,7 @@ class Foliation(GeometricModel):
                     leaves = self.compute_leaf(y0, transverse=transverse)
                     plt.plot(leaves[0], leaves[1], ":", color='blue', zorder=1)
                 except ValueError:
-                    J = self.jac_proba(torch.tensor(y0).float().unsqueeze(0)).squeeze(0).detach()
+                    J = self.jac_proba(torch.tensor(y0).to(self.device).to(self.dtype).unsqueeze(0)).squeeze(0).detach()
                     print(f'Failed with {y0} and \njacobian {J}')
                     
                 # print(leaves)

geometry.py
@@ -1,10 +1,11 @@
 """Module implementing tools to examine the geometry of a model."""
+from sympy import euler
 import torch
 from torch import nn
 from torch.autograd.functional import jacobian, hessian
 from tqdm import tqdm
 from scipy.integrate import solve_ivp
-from torchdiffeq import odeint_event
+from torchdiffeq import odeint, odeint_event
 
 
 class GeometricModel(object):
@@ -12,7 +13,7 @@ class GeometricModel(object):
     def __init__(self,
                  network: nn.Module,
                  network_score: nn.Module,
-                 verbose: bool=True,
+                 verbose: bool=False,
     ) -> None:
 
         super(GeometricModel, self).__init__()
@@ -21,12 +22,13 @@ class GeometricModel(object):
         # self.network.eval()
         self.verbose = verbose
         self.device = next(self.network.parameters()).device
+        self.dtype = next(self.network.parameters()).dtype
 
 
     def proba(
         self,
         eval_point: torch.Tensor,
-    ) -> None:
+    ) -> torch.Tensor:
 
         if len(eval_point.shape) == 3:  # TODO: trouver un truc plus propre
             eval_point = eval_point.unsqueeze(0)
@@ -37,7 +39,7 @@ class GeometricModel(object):
     def score(
         self,
         eval_point: torch.Tensor,
-    ) -> None:
+    ) -> torch.Tensor:
         
         if len(eval_point.shape) == 3:  # TODO: trouver un truc plus propre
             eval_point = eval_point.unsqueeze(0)
@@ -301,6 +303,7 @@ class GeometricModel(object):
                         J_s, torch.diag_embed(J_p.mT) - pdp - pdp.mT, J_s
                     )
         else:
+            raise NotImplementedError
             def G(x): return self.local_data_matrix(x, create_graph=True)
             if self.verbose:
                 print(f"shape of eval_point = {eval_point.shape}")
@@ -318,40 +321,81 @@ class GeometricModel(object):
     ) -> torch.Tensor:
         J_G = self.jac_metric(eval_point)
         G = self.local_data_matrix(eval_point)
-        G_inv = torch.linalg.pinv(G.to(torch.double), hermitian=True).float() # input need to be in double
+        G_inv = torch.linalg.pinv(G.to(torch.double), hermitian=True).to(self.dtype) # input need to be in double
 
         return torch.einsum(
                     "...kl, ...ijl -> ...ijk",
                     G_inv, J_G + J_G.permute(0, 2, 1, 3) - J_G.permute(0, 3, 1, 2)
                 ) / 2
 
-    
-
     def geodesic(
         self,
         eval_point: torch.Tensor,
         init_velocity: torch.Tensor,
         euclidean_budget: float=None,
+        full_path: bool=False,
     ) -> torch.Tensor:
+        if len(init_velocity.shape) > 2:
+            init_velocity = init_velocity.flatten(1)
         
         def ode(t, y):
-            x, v = y[0], y[1].flatten(1)
+            x, v = y
             christoffel = self.christoffel(x)
-            return torch.stack((v.reshape(x.shape), torch.einsum("...i, ...j, ...ijk -> ...k", v, v, christoffel).reshape(x.shape)))
+            return (v.reshape(x.shape), torch.einsum("...i, ...j, ...ijk -> ...k", v, v, - christoffel))
         
-        def euclidean_stop(t, y):
-            if euclidean_budget is None:
-                return nn.ReLU(2. - t)
-            x = y[0]
-            return torch.norm(x - eval_point).pow(2) - euclidean_budget
-        
-        euclidean_stop.terminal = True
-        
-        y0 = torch.stack((eval_point, init_velocity)) # TODO: wrong dim after bash -> should be flatten ?
+        if euclidean_budget is None:
+            y0 = (eval_point, init_velocity) # TODO: wrong dim after bash -> should be flatten ?
+
+            solution_ode = odeint(ode, y0, t=torch.linspace(0., 4., 1000), method="rk4")
+            solution_ode_x, solution_ode_v = solution_ode
+
+            return solution_ode_x[-1]
+
+        elif euclidean_budget <= 0.:
+            return eval_point
+
+        else:
+            def euclidean_stop(t, y):
+                x = y[0]
+                return nn.functional.relu(euclidean_budget - torch.norm(x - eval_point, dim=1))
+            if self.verbose:
+                print(f"eval_point: {eval_point.shape}")
+                print(f"init_velocity: {init_velocity.shape}")
+            y0 = (eval_point, init_velocity)
+                
+            # solution_ivp = solve_ivp(ode, t_span = (0, 2), y0=(eval_point.detach().numpy(), init_velocity.detach().numpy()), method='RK23', events=euclidean_stop if euclidean_budget is not None else None)
+
+            # event_t, solution_ode = odeint_event(ode, y0, t0=torch.tensor(0.), event_fn=euclidean_stop, atol=euclidean_budget / 100, method="rk4", options={"step_size": euclidean_budget / 1000})
             
-        # solution_ivp = solve_ivp(ode, t_span = (0, 2), y0=(eval_point.detach().numpy(), init_velocity.detach().numpy()), method='RK23', events=euclidean_stop if euclidean_budget is not None else None)
+            # if self.verbose: print(f"event_t: {event_t}")
 
-        event_t, solution_ode = odeint_event(ode, y0, t0=torch.tensor(0.), event_fn=euclidean_stop, atol=euclidean_budget / 1000)
+            solution_ode = odeint(ode, y0, t=torch.linspace(0., 4., int(100 / euclidean_budget) ), method="rk4")
+            
+            # self.verbose = True
+            solution_ode_x, solution_ode_v = solution_ode
+            if full_path:
+                return solution_ode_x.transpose(0, 1)
+            
+            if self.verbose:
+                print(f"solution_ode_x: {solution_ode_x.shape}")
+                print(f"solution_ode_v: {solution_ode_v.shape}")
+                print(f"0 is initial value ? {torch.allclose(solution_ode_x[0], eval_point)} dist: {torch.dist(solution_ode_x[0], eval_point)}")
 
+            # Get last point exceeding the euclidean budget
+            admissible_indices = ((solution_ode_x - eval_point.unsqueeze(0)).norm(dim=-1) <= euclidean_budget)
+            last_admissible_index = admissible_indices.shape[0] - 1 - admissible_indices.flip(dims=[0]).int().argmax(dim=0)
+            last_admissible_solution_x = torch.diagonal(solution_ode_x[last_admissible_index]).T
+                
+            if self.verbose:
+                last_admissible_solution_x_loop = torch.zeros_like(eval_point)
+                last_admissible_index_loop = torch.zeros(eval_point.shape[0])
 
-        return solution_ode
+                for i, step in enumerate(solution_ode_x):
+                    for j, batch in enumerate(step):
+                        if (batch - eval_point[j]).norm() <= euclidean_budget:
+                            last_admissible_index_loop[j] = i
+                            last_admissible_solution_x_loop[j] = batch
+                print(f"2 solutions are the same ? {torch.allclose(last_admissible_solution_x, last_admissible_solution_x_loop)}")
+                print(f"2 indices of solutions are the same ? {torch.allclose(last_admissible_index.int(), last_admissible_index_loop.int())}")
+                        
+            return last_admissible_solution_x

main.py
@@ -7,8 +7,9 @@ import torch
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-from adversarial_attack import (APGDAttack, AdversarialAutoAttack, OneStepSpectralAttack,
+from adversarial_attack import (APGDAttack, AdversarialAutoAttack, GeodesicSpectralAttack, OneStepSpectralAttack,
                                 TwoStepSpectralAttack)
 from adversarial_attack_plots import compare_fooling_rates, compare_inf_norm, plot_attacks_2D, plot_contour_2D, plot_curvature_2D
 import mnist_networks, cifar_networks
@@ -67,7 +68,7 @@ if __name__ == "__main__":
         metavar="attack",
         nargs="+",
         default=["TSSA", "OSSA"],
-        choices=["TSSA", "OSSA", "AA", "APGD"],
+        choices=["TSSA", "OSSA", "AA", "APGD", "GSA"],
         help="List of attacks to run on the task."
     )
     parser.add_argument(
@@ -120,6 +121,7 @@ if __name__ == "__main__":
     attack_paths = args.attacks
     xor_data_range = args.range
     precision_type = torch.double if args.double else torch.float
+    # torch.set_default_dtype(precision_type)
 
     batch_size = 125
     savedirectory = args.savedirectory + ("" if args.savedirectory[-1] == '/' else '/') + f"{dataset_name}/{task}/"
@@ -149,6 +151,12 @@ if __name__ == "__main__":
     elif dataset_name[:3] == "XOR":
         MAX_BUDGET = 0.5
         STEP_BUDGET = 0.005
+        
+    if "GSA" in attacks_to_run_str:
+        MAX_BUDGET = 1
+        STEP_BUDGET = 0.2
+        batch_size = 2
+
     
     if task in ["plot-curvature", "plot-contour"] and dataset_name[:3] != 'XOR':
         raise ValueError(f'Plot curvature requires a 2D dataset and not {dataset_name}.')
@@ -270,6 +278,12 @@ if __name__ == "__main__":
                         network_score=network_score,
             )
             attacks_to_run.append(APGD)
+        if attack_name.casefold() == 'gsa':
+            GSA = GeodesicSpectralAttack(
+                        network=network,
+                        network_score=network_score,
+            )
+            attacks_to_run.append(GSA)
         
     print(f"attacks created: {[type(a).__name__ for a in attacks_to_run]}")
     
@@ -286,7 +300,7 @@ if __name__ == "__main__":
         print("Loading precomputed attacks", end='')
         for attack_path in attack_paths:
             print(".", end='')
-            br, ip, av = torch.load(attack_path, map_location=device)
+            br, ip, av = torch.load(attack_path, map_location=device, dtype=precision_type)
             budget_range_list.append(br)
             input_points_list.append(ip)
             attack_vectors.append(av)
@@ -324,9 +338,9 @@ if __name__ == "__main__":
         input_points = input_points.to(device).to(precision_type)
 
     # result = TSSA.jac_metric(input_points)
-    geo = TSSA.geodesic(input_points, OSSA.compute_attack(input_points, 1e-1) - input_points, euclidean_budget=1e-1)
-    print(geo)
-    exit()
+    # geo = TSSA.geodesic(input_points, OSSA.compute_attack(input_points, 1e-1) - input_points, euclidean_budget=1e-1)
+    # print(geo)
+    # exit()
 
     if task == "plot-attack":
         plt.matshow(input_points[0][0])
@@ -335,18 +349,26 @@ if __name__ == "__main__":
             attack_output = adversarial_attack.compute_attack(input_points, budget=1)
             plt.matshow(attack_output.detach().numpy()[0][0])
             plt.show()
-    
-    if task == "save-attacks":
-        savename = f"attacks_nsample={num_samples}_start={start_index}"
-        savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
-
-        if device.type == 'cuda':
+            
+    # We split data or not depending on the ram usage to avoid excess (on our computer)
+    if task in ["save-attacks", "fooling-rates", "inf-norm"]:
+        if attack_paths is not None:
+            print("No batches.")
+            batched_input_points = [input_points]
+        elif (device.type == 'cuda') or ('GSA' in attacks_to_run_str):
+            print(f"Batch of size {batch_size}.")
             batched_input_points = torch.split(input_points, batch_size , dim=0)
         elif dataset_name in ["MNIST", "XOR", "XOR-old"]: # enough memory in cpu for a single batch
+            print("No batches.")
             batched_input_points = [input_points]
         else:
             batch_size = 200
+            print(f"Batch of size {batch_size}.")
             batched_input_points = torch.split(input_points, batch_size, dim=0)
+
+    if task == "save-attacks":
+        savename = f"attacks_nsample={num_samples}_start={start_index}_nl={non_linearity}"
+        savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
 
         for batch_index, batch in enumerate(batched_input_points):
             print(f"Batch number {batch_index} starting...")
@@ -363,16 +385,6 @@ if __name__ == "__main__":
         savename = f"fooling_rates_compared_nsample={num_samples}_start={start_index}_nl={non_linearity}"
         savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
 
-        if attack_paths is not None:
-            batched_input_points = [input_points]
-        elif device.type == 'cuda':
-            batched_input_points = torch.split(input_points, batch_size , dim=0)
-        elif dataset_name in ["MNIST", "XOR", "XOR-old"]: # enough memory in cpu for a single batch
-            batched_input_points = [input_points]
-        else:
-            batch_size = 200
-            batched_input_points = torch.split(input_points, batch_size, dim=0)
-
         for batch_index, batch in enumerate(batched_input_points):
             print(f"Batch number {batch_index} starting...")
             compare_fooling_rates(
@@ -384,19 +396,8 @@ if __name__ == "__main__":
             )
 
     if task == "inf-norm":
-        savename = f"inf_norm_compared_nsample={num_samples}"
+        savename = f"inf_norm_compared_nsample={num_samples}_nl={non_linearity}"
         savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
-
-        if attack_paths is not None:
-            batched_input_points = [input_points]
-        elif device.type == 'cuda':
-            batched_input_points = torch.split(input_points, batch_size , dim=0)
-        elif dataset_name in ["MNIST", "XOR", "XOR-old"]: # enough memory in cpu for a single batch
-            batched_input_points = [input_points]
-        else:
-            batch_size = 200
-            batched_input_points = torch.split(input_points, batch_size, dim=0)
-
 
         for batch_index, batch in enumerate(batched_input_points):
             print(f"Batch number {batch_index} starting...")
@@ -412,12 +413,16 @@ if __name__ == "__main__":
         colors_dict = {'tssa': 'blue',
                        'ossa': 'orange',
                        'aa': 'red',
-                       'apdg': 'green'}
-        foliation.plot(eigenvectors=False)
+                       'apgd': 'green',
+                       'gsa': 'purple'}
+        foliation.plot(eigenvectors=False, transverse=True)
         for name, adversarial_attack in zip(attacks_to_run_str, attacks_to_run):
-            plot_attacks_2D(adversarial_attack, test_points=input_points,budget=0.1, color=colors_dict[name.casefold()])
+            plot_attacks_2D(adversarial_attack, test_points=input_points, budget=0.3, color=colors_dict[name.casefold()])
             # foliation.plot(eigenvectors=False)
             # plt.legend()
+        geodesics = GSA.geodesic(eval_point=input_points, init_velocity=OSSA.compute_attack(input_points, budget=0.3) - input_points, euclidean_budget=0.3, full_path=True)
+        for geodesic in geodesics:
+            plt.plot(geodesic[:, 0].detach().numpy(), geodesic[:, 1].detach().numpy(), ":", color='red')
         savename = f"plot_attacks_2D_budget=1e-1_nsample={num_samples}_nl={non_linearity}"
         savepath = savedirectory + ("" if savedirectory[-1] == "/" else "/") + savename
         plt.savefig(savepath + '.pdf', format='pdf')